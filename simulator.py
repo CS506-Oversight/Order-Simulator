97c27a660876ff26358c951ee4b8f0c96f324336
Joziah Mays <jmays2@wisc.edu>
2021-04-26 20:14:59 -0500

    Fixed order stuff

--- simulator.py
+++ simulator.py
@@ -3,25 +3,35 @@ import json
 import random
 
 def random_order(data):
-   # print(data)
-   order_list=[]
-   for item in data:
-        rand_num = random.randint(0, 1)
-        if (rand_num == 1):
-            rand_amt = random.randint(1,5)
-            order_list.append({item['id']: str(rand_amt), "item_name":item['name']})
-   return (order_list)
+    order_list=[]
+    picked = []
+    n = len(data)
+    
+    rand_len = random.randint(1, n)
+    
+    for i in range(rand_len):
+        rand_item_idx = random.randint(0, n-1)
+        
+        while rand_item_idx in picked:
+            rand_item_idx = random.randint(0, n-1)
+            
+        picked.append(rand_item_idx)
+        item = data[rand_item_idx]
+        rand_amt = random.randint(1,3)
+        
+        order_list.append({"menu_id": item['id'], "amount": str(rand_amt), "item_name":item['name']})
+    
+    return (order_list)
 
 if __name__ == '__main__':
-    user = {}
-    user['id'] = 'kjhgvjklerlsjoife'
-    response = requests.get('http://127.0.0.1:5000/menu-item', json=user)
-    # data = json.load(response.json()['data'])
+    user_id = 'jRD9JUG1fzMP3FoMopfIivfgBh42'
+    response = requests.get(f'http://127.0.0.1:5000/menu-item?user_id={user_id}')
+    print(json.dumps(response.json(), indent=4))
     data = response.json()['data']
-    print(data)
+    # print(data)
     order_list = random_order(data)
-    order_data = {"payload": order_list, "user_id": 'kjhgvjklerlsjoife'}
+    order_data = {"payload": order_list}
     json_send = json.dumps(order_data)
     print(json_send)
-    response2 = requests.post('http://127.0.0.1:5000/order', data=json_send)
-    print(response2)
+    response2 = requests.post(f'http://127.0.0.1:5000/order?user_id={user_id}', data=json_send)
+    print(json.dumps(response2.json(), indent=4))
